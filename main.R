357026a827d6cb5bcbac85d69e8adbce53829002
Tom Teman <tomteman42@gmail.com>
2018-05-17 19:36:05 +0300

    working with data

--- main.R
+++ main.R
@@ -3,7 +3,9 @@ rm(list=ls())
 
 getwd()
 
-pf <- read.csv("bike_train.csv", sep = ",")
+bike_train <- read.csv("bike_train.csv", sep = ",")
 
+bike_train$datetime <- as.POSIXct(bike_train$datetime, tz = "", format = "%Y-%m-%d%H:%M:%S")
 
+bike_train$season <- factor(bike_train$season, labels = c("Winter", "Spring", "Summer", "Fall"))
 
