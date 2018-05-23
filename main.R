cc7e338f6c6900507a3d9fa5b3794b1d9bcee1fc
Tom Teman <tomteman42@gmail.com>
2018-05-23 21:00:44 +0300

    .

--- main.R
+++ main.R
@@ -2,9 +2,9 @@
 # install.packages("lubridate", dependencies = TRUE)
 # install.packages("ggplot", dependencies = TRUE)
 #install.packages("plyr", dependencies = TRUE)
-#library(ggplot2)
-#library(lubridate)
-#library(randomForest)
+library(ggplot2)
+library(lubridate)
+library(randomForest)
 
 # clean
 rm(list=ls())
@@ -16,6 +16,7 @@ bike_test <- read.csv("bike_test.csv", sep = ",")
 
 # format datetime column
 bike_train$datetime <- as.POSIXct(bike_train$datetime, tz = "UTC", format = "%Y-%m-%d%H:%M:%S")
+bike_test$datetime <- as.POSIXct(bike_test$datetime, tz = "UTC", format = "%Y-%m-%d%H:%M:%S")
 
 # initial histograms for overview of data
 par(mfrow=c(4,2));
@@ -31,16 +32,29 @@ hist(bike_train$workingday);
 
 # factorize non-continuous columns
 bike_train$season_factored <- factor(bike_train$season, labels = c("Winter", "Spring", "Summer", "Fall"))
-bike_train$holidy <- factor(bike_train$holiday, labels = c("Not_Holiday", "Holiday"))
+bike_train$holiday_factored <- factor(bike_train$holiday, labels = c("Not_Holiday", "Holiday"))
 bike_train$workingday_factored <- factor(bike_train$workingday, labels = c("Not_Working_Day", "Working_Day"))
 bike_train$weather_factored <- factor(bike_train$weather, labels = c("Good", "Normal", "Bad", "Very Bad"))
+
+
+bike_test$season_factored <- factor(bike_test$season, labels = c("Winter", "Spring", "Summer", "Fall"))
+bike_test$holiday_factored <- factor(bike_test$holiday, labels = c("Not_Holiday", "Holiday"))
+bike_test$workingday_factored <- factor(bike_test$workingday, labels = c("Not_Working_Day", "Working_Day"))
+bike_test$weather_factored <- factor(bike_test$weather, labels = c("Good", "Normal", "Bad"))
+
 
 # split up datetime into components
 bike_train$month <- as.integer(format(as.POSIXlt(bike_train$datetime), format = "%m"))
 bike_train$weekday <- as.integer(format(as.POSIXlt(bike_train$datetime), format = "%u"))
-bike_train$day_name=wday(as.Date(bike_train$datetime,"%Y-%m-%d%H:%M:%S", tz = "UTC"), label=TRUE)
 bike_train$hour <- as.integer(format(as.POSIXlt(bike_train$datetime), format = "%H"))
+bike_train$day_name=wday(as.Date(bike_train$datetime,"%Y-%m-%d%H:%M:%S", tz = "UTC"), label=TRUE)
 bike_train$hour_factored <- as.factor(bike_train$hour)
+
+bike_test$month <- as.integer(format(as.POSIXlt(bike_test$datetime), format = "%m"))
+bike_test$weekday <- as.integer(format(as.POSIXlt(bike_test$datetime), format = "%u"))
+bike_test$hour <- as.integer(format(as.POSIXlt(bike_test$datetime), format = "%H"))
+bike_test$day_name=wday(as.Date(bike_test$datetime,"%Y-%m-%d%H:%M:%S", tz = "UTC"), label=TRUE)
+bike_test$hour_factored <- as.factor(bike_test$hour)
 
 # boxplot relation between hour and count of rentals
 boxplot(bike_train$count~bike_train$hour_factored,xlab="hour", ylab="count of rentals", col=rainbow(length(unique(bike_train$hour_factored))),outline=FALSE)
@@ -75,21 +89,15 @@ boxplot(main="Relation between hour and count of rentals in fall", bike_train_fi
 # boxplot relation between hour and count of rentals in non-workdays
 boxplot(bike_train$count~bike_train$hour_factored,xlab="hour", ylab="count of rentals", col=rainbow(length(unique(bike_train$hour_factored))),outline=FALSE)
 
-
 # plot relation between count and date, with points colored according to temp
-
-bike_train$datetime <- as.POSIXct(bike_train$datetime)
 pl <- ggplot(bike_train,aes(datetime,count)) + geom_point(aes(color=temp),alpha=0.5)
 pl + ggtitle("Relation between count and date, with points colored according to temperature") + scale_color_continuous(low = '#55D8CE',high = '#FF6E2E') + theme_bw()
-
-
 
 # section 2
 bikes_train_lm <- lm(data = bike_train, count ~ temp)
 summary(bikes_train_lm)
 plot(bike_train$temp, bike_train$count, pch = 20, cex = .5, col = "blue", main = "Count plotted against temperature", xlab = "Temperature", ylab = "Count")
 abline(lm(data = bike_train, count ~ temp))
-
 
 ## 70% of the sample size
 smp_size <- floor(0.7 * nrow(bike_train))
@@ -113,6 +121,14 @@ bike_train$hour_window<-ifelse(bike_train$hour>=10 & bike_train$hour<=15,"midday
 bike_train$hour_window<-ifelse(bike_train$hour>=16 & bike_train$hour<=19,"evening commute", bike_train$hour_window)
 bike_train$hour_window<-ifelse(bike_train$hour>=20 & bike_train$hour<=22,"late evening", bike_train$hour_window)
 bike_train$hour_window <- as.factor(bike_train$hour_window)
+
+bike_test$hour_window<-NA
+bike_test$hour_window<-ifelse(bike_test$hour>=0 & bike_test$hour<=6 | bike_test$hour==23,"night", bike_test$hour_window)
+bike_test$hour_window<-ifelse(bike_test$hour>=7 & bike_test$hour<=9,"morning commute", bike_test$hour_window)
+bike_test$hour_window<-ifelse(bike_test$hour>=10 & bike_test$hour<=15,"midday", bike_test$hour_window)
+bike_test$hour_window<-ifelse(bike_test$hour>=16 & bike_test$hour<=19,"evening commute", bike_test$hour_window)
+bike_test$hour_window<-ifelse(bike_test$hour>=20 & bike_test$hour<=22,"late evening", bike_test$hour_window)
+bike_test$hour_window <- as.factor(bike_test$hour_window)
 
 
 # create hour windows in training
@@ -171,17 +187,22 @@ bike_train$atemp_normalized<-ifelse(bike_train$atemp<=32, bike_train$atemp*100/3
 subset_test$atemp_normalized<-NA
 subset_test$atemp_normalized<-ifelse(subset_test$atemp>32, 60+(46-subset_test$atemp)*40/14, subset_test$atemp_normalized);
 subset_test$atemp_normalized<-ifelse(subset_test$atemp<=32, subset_test$atemp*100/32, subset_test$atemp_normalized);
+bike_test$atemp_normalized<-NA
+bike_test$atemp_normalized<-ifelse(bike_test$atemp>32, 60+(46-bike_test$atemp)*40/14, bike_test$atemp_normalized);
+bike_test$atemp_normalized<-ifelse(bike_test$atemp<=32, bike_test$atemp*100/32, bike_test$atemp_normalized);
 
 # create agg_temp variable in datasets
 bike_train$agg_temp = NA
 bike_train$agg_temp = (bike_train$atemp_normalized - bike_train$humidity)*4 - bike_train$weather*10 + bike_train$season*25
 subset_test$agg_temp = NA
 subset_test$agg_temp = (subset_test$atemp_normalized - subset_test$humidity)*4 - subset_test$weather*10 + subset_test$season*25
+bike_test$agg_temp = NA
+bike_test$agg_temp = (bike_test$atemp_normalized - bike_test$humidity)*4 - bike_test$weather*10 + bike_test$season*25
 
 # see correlation of agg_temp to count
 cor(bike_train$count, bike_train$agg_temp)
 
-bike_train.final <- lm(data = bike_train, count ~ holiday*hour_window+workingday_factored*hour_window+hour*hour_window+agg_temp*hour_window)
+bike_train.final <- lm(data = bike_train, count ~ holiday_factored*hour_window+workingday_factored*hour_window+hour*hour_window+agg_temp*hour_window)
 summary(bike_train.final)
 
 subset_test$predictTest = predict(bike_train.final, subset_test)
@@ -190,5 +211,8 @@ final.sst_test = sum((subset_test$count - mean(bike_train$count))^2)
 1 - final.sse_test/final.sst_test
 
 # add all new columns to bike_test so we can run our prediction 
-
+bike_test$count = floor(predict(bike_train.final, bike_test))
+#cleaning up outliers
+bike_test$count = ifelse(bike_test$count < 0, 0, bike_test$count)
+write.csv(bike_test, file = "bike_test.csv")
 
